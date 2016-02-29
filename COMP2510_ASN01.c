affde7db2e1548cb097b38421328e9609aa88a34
DericM <dmccadden@my.bcit.ca>
2016-02-28 22:42:22 -0800

    c

--- COMP2510_ASN01.c
+++ COMP2510_ASN01.c
@@ -10,6 +10,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdlib.h>
 #define LINESIZE 1024
 #define CHECK(PRED) printf("%s ... %s\n",(PRED) ? "passed" : "FAILED", #PRED)
 
@@ -19,17 +20,41 @@ void print_menu();
 
 int main(int argc, char *argv[]) {
 
+	print_menu();
+
+	int c;
 
 
 
 
+	printf("");
+	while (1) {
+		make_choics(get_choice());
+	}
 
 
+
+
+	/*
+	int c;
+	while ((c = getchar()) != EOF)
+		;
+
+	FILE *fp = "file.txt";
+	int c;
+	while ((c = fgetc(fp)) != EOF)
+		;
+
+	char line[LINESIZE];
+	while (fgets(line, LINESIZE, stdin))
+		;
+	*/
 	return 0;
 }
 
 
-/* prints a simple menu
+/* 
+prints a simple menu
 */
 void print_menu() {
 	printf("--------Menu--------\n");
@@ -39,4 +64,30 @@ void print_menu() {
 	printf("Modify-Record-n-( n)\n\n");
 }
 
+/*
+Return 1 if a valid choice has been made, if not return 0;
+*/
+int make_choics(int choice) {
+	if (choice == -2)
+		exit(1);
+	else if(choice == -1)
+		append();
+	else if (choice == 0)
+		display_all();
+	else if (choice >= 1)
+		modify();
+	else
+		return 0;
+	return 1;
+}
 
+
+int get_choice() {
+	int c;
+	while ((c = getchar()) != EOF)
+		return c;
+
+	c = getchar();
+
+	return -2;
+}
